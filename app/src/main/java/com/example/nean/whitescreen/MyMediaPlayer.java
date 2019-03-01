d6b5339082164a00fdbb00a63f476e3fb335aa90
nean <nean.lu@goertek.com>
2019-03-01 10:44:38 +0800

    Delete useless class.

--- app/src/main/java/com/example/nean/whitescreen/MyMediaPlayer.java
+++
@@ -1,9 +0,0 @@
-package com.example.nean.whitescreen;
-
-import android.media.MediaPlayer;
-import android.view.SurfaceView;
-
-public class MyMediaPlayer {
-    private MediaPlayer mMediaPlayer;
-    private SurfaceView mSurfaceView;
-}
